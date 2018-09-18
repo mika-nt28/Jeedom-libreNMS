ad35017ca9a24f11b9151495022f95b0ef8e775f
Halbout Michael <michaelhalbout@gmail.com>
2018-09-18 12:10:11 +0200

    Update libreNMS.ajax.php

--- core/ajax/libreNMS.ajax.php
+++ core/ajax/libreNMS.ajax.php
@@ -1,20 +1,17 @@
 <?php
 try {
 	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    	include_file('core', 'authentification', 'php');
-	include_file('core', 'dpt', 'class', 'eibd');
     	if (!isConnect('admin')) {
         	throw new Exception(__('401 - Accès non autorisé', __FILE__));
     	}
-	if (init('action') == 'getLog') {
-  
-				$eqLogic = new libreNMS();
-        $eqLogic->getDevice();
-		    ajax::success("L'import de device a ete executé");
+	if (init('action') == 'getDevice') {
+		$eqLogic = new libreNMS();
+		$eqLogic->getDevice();
+		ajax::success("L'import de device a ete executé");
 	}
-   throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
     /*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
+	ajax::error(displayExeption($e), $e->getCode());
 }
 ?>
