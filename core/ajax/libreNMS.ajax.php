e9c2e786366038e1ec06c64e76ea00f02330319c
Halbout Michael <michaelhalbout@gmail.com>
2018-09-20 11:16:17 +0200

    Update libreNMS.ajax.php

--- core/ajax/libreNMS.ajax.php
+++ core/ajax/libreNMS.ajax.php
@@ -1,9 +1,10 @@
 <?php
 try {
 	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    	if (!isConnect('admin')) {
-        	throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    	}
+	include_file('core', 'authentification', 'php');
+	if (!isConnect('admin')) {
+		throw new Exception(__('401 - Accès non autorisé', __FILE__));
+	}
 	if (init('action') == 'getDevice') {
 		libreNMS::getDevice();
 		ajax::success("L'import de device a ete executé");
