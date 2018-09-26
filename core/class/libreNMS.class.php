8c9a3de069bae96e6a17df6ec4eb17efd453ea54
Halbout Michael <michaelhalbout@gmail.com>
2018-09-26 10:04:28 +0200

    Update libreNMS.class.php

--- core/class/libreNMS.class.php
+++ core/class/libreNMS.class.php
@@ -73,17 +73,16 @@ class libreNMS extends eqLogic {
 			}
 		}
 	}
-  	public static function getDeviceHealth($deviceName) {
-		$result=self::Request('/api/v0/devices/'.$deviceName.'/health');
-		foreach($result['graphs'] as $graphs){
-			
-
-				log::add('libreNMS','debug','commande: '.$graphs['name'].'est trouvée');
-          
-		
-		}
-	}
 	/*     * *********************Methode d'instance************************* */
+  	public function getDeviceHealth() {
+		$Graph=array();
+		$result=self::Request('/api/v0/devices/'.$this->getName().'/health');
+		foreach($result['graphs'] as $graphs){
+			log::add('libreNMS','debug','commande: '.$graphs['name'].'est trouvée');
+			$Graph[$graphs["desc"]]=self::Request('/api/v0/devices/'.$this->getName().'/health/'.$graphs["name"]);
+		} 
+		return $Graph;
+	}
 	public function getSystem() {
 		$Result=self::Request('/api/v0/system');
 		if($Result["status"] == "ok"){
