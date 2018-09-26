0320f9da6b2ab52b633a264dbef6873cb7cdf3d7
Halbout Michael <michaelhalbout@gmail.com>
2018-09-26 09:39:19 +0200

    Update libreNMS.php

--- desktop/php/libreNMS.php
+++ desktop/php/libreNMS.php
@@ -31,6 +31,12 @@
 				</center>
 				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#406E88"><center>Ajouter</center></span>
 			</div>
+			<div class="cursor libreNMSAction" data-action="import" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 5em;color:#406E88;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#406E88"><center>Ajouter</center></span>
+			</div>
 			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
 				<center>
 					<i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
